3f7b9d6359b0221221c400cf505b6bd907ede014
lovegupta112 <loveguptaofficials@gmail.com>
2023-07-19 17:54:04 +0000

    updated

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -6,6 +6,7 @@ const Child=({changeShowModal,modal})=>{
     return (
         <>
         <div style={{backgroundColor:'#7f1a1a',padding:'1rem'}}>
+            
             <h2>Child Component</h2>
             <button onClick={()=>changeShowModal(true)}>Show Modal</button>
             {modal && <div>
