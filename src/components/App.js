fe66fbae184ca14b18c7c1a9317354ec70dfb4d4
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-07-19 17:47:03 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import Child from "./Child";
 const App = () => {
+
+  const [showModal,setShowModal]=useState(false);
   return (
-    <div>
+    <div style={{backgroundColor:'purple' ,padding:'1rem'}}>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+         <Child changeShowModal={setShowModal} modal={showModal}/>
+         
     </div>
   )
 }
---
+++ src/components/Child.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Child=({changeShowModal,modal})=>{
+
+    return (
+        <>
+        <div style={{backgroundColor:'#7f1a1a',padding:'1rem'}}>
+            <h2>Child Component</h2>
+            <button onClick={()=>changeShowModal(true)}>Show Modal</button>
+            {modal && <div>
+          <h2>Modal Content</h2>
+          <p>This is the modal Content</p>
+         </div>}
+        </div>
+        </>
+    )
+}
+export default Child;
